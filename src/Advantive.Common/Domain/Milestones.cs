624f483765102a8c45c01d87bb6f3d732532cd23
Greg Cook <greg.cook@nutrien.com>
2025-02-23 10:58:00 -0700

    xer-1 - able to create a database on the sql container

--- src/Advantive.Common/Domain/Milestones.cs
+++ src/Advantive.Common/Domain/Milestones.cs
@@ -9,5 +9,5 @@ public class Milestones : AuditImmutableBase
     public string Description { get; set; } = string.Empty;
     public DateTime? DueDate { get; set; }
 
-    public Project Project { get; set; }
+    public Project Project { get; set; } = null!;
 }
